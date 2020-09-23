04b39b9e5d6b7c47419ed430ef381124ea27da85
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-09-23 14:23:41 -0700

    findById studio

--- __tests__/studio.test.js
+++ __tests__/studio.test.js
@@ -13,4 +13,12 @@ describe('Studio routes', () => {
 
     expect(response.body).toEqual(expect.arrayContaining(studios));
   });
+
+  it('gets a studio by id via GET', async() => {
+    const studio = (await Studio.find())[0];
+    const response = await request(app)
+      .get(`/api/v1/studios/${studio.id}`);
+  
+    expect(response.body).toEqual(studio);
+  });
 });

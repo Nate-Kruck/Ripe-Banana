480aaa656ca72db17d8ac80c5d281a64e4feb9a7
Adrian Simental <asimental100@hotmail.com>
2020-09-25 17:34:08 -0700

    done with actor by id test

--- __tests__/actor.test.js
+++ __tests__/actor.test.js
@@ -1,3 +1,5 @@
+const pool = require('../lib/utils/pool');
+
 require('../lib/data/data-helpers');
 const request = require('supertest');
 const app = require('../lib/app');
@@ -16,11 +18,20 @@ describe('Actor routes', () => {
   });
 
   it('gets a actor by id via GET', async() => {
+    
     const actor = (await Actor.find())[0];
-    const response = await request(app)
-      .get(`/api/v1/actors/${actor.id}`);
-  
-    expect(response.body).toEqual({ ...actor, dob: actor.dob.toISOString() });
+    const response = await request(app).get(`/api/v1/actors/${actor.id}`);
+    
+    const actorFilms = await pool.query(
+      `SELECT films.id as film_id, films.title, films.released
+      FROM films 
+      INNER JOIN characters 
+      ON films.id=characters.film
+      WHERE characters.actor=$1
+      `, [1]
+    );
+    
+    expect(response.body).toEqual({ ...actor, dob: actor.dob.toISOString(), films: actorFilms.rows });
   });
 
   it('creates a actor', async() => {
